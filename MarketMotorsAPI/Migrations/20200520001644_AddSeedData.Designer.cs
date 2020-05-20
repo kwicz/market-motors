f53c00bb9f2002bee414e1ce0ad3395ec5b9fcfe
Katy Solovewicz <katy@solovewicz.com>
2020-05-19 17:21:14 -0700

    add featured column and seed data to database

--- MarketMotorsAPI/Migrations/20200520001644_AddSeedData.Designer.cs
+++ MarketMotorsAPI/Migrations/20200520001644_AddSeedData.Designer.cs
@@ -2,14 +2,16 @@
 using MarketMotors.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MarketMotors.Migrations
 {
     [DbContext(typeof(MarketMotorsContext))]
-    partial class MarketMotorsContextModelSnapshot : ModelSnapshot
+    [Migration("20200520001644_AddSeedData")]
+    partial class AddSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MarketMotorsAPI/Models/MarketMotorsContext.cs
+++ MarketMotorsAPI/Models/MarketMotorsContext.cs
@@ -12,29 +12,71 @@ namespace MarketMotors.Models
     public DbSet<Vehicle> Vehicles { get; set; }
     public DbSet<Feature> Features { get; set; }
 
-    // protected override void OnModelCreating(ModelBuilder builder)
-    // {
-    //   builder.Entity<Vehicle>()
-    //       .HasData(
-    //           new Vehicle   {
-    //               VehicleId = 1,
-    //               VehicleTitle = "1998 Chevy S10",
-    //               Vin = "79879851655",
-    //               StockNumber = "1655",
-    //               Make = "Chevy",
-    //               Model = "Model",
-    //               Year = 1998,
-    //               Condition = "Good",
-    //               Price = "2,000",
-    //               Availability = "Available",
-    //               ExteriorColor = "Taupe",
-    //               InteriorColor = "Taupe",
-    //               Doors = "3",
-    //               FuelType = "Gasoline",
-    //               Engine = "V8",
-    //               Transmission = "Automatic",
-    //               Mileage = "100,000",
-    //             });
-    // }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Vehicle>()
+          .HasData(
+              new Vehicle   {
+                  VehicleId = 1,
+                  VehicleTitle = "1998 Chevy S10",
+                  Featured = "true",
+                  Vin = "79879851655",
+                  StockNumber = "1655",
+                  Make = "Chevy",
+                  Model = "Model",
+                  Year = "1998",
+                  Condition = "Good",
+                  Price = "2,000",
+                  Availability = "Available",
+                  ExteriorColor = "Taupe",
+                  InteriorColor = "Taupe",
+                  Doors = "3",
+                  FuelType = "Gasoline",
+                  Engine = "V8",
+                  Transmission = "Automatic",
+                  Mileage = "100,000"
+                },
+              new Vehicle   {
+                  VehicleId = 2,
+                  VehicleTitle = "2006 Honda Fit",
+                  Featured = "true",
+                  Vin = "98765432198765432",
+                  StockNumber = "5432",
+                  Make = "Honda",
+                  Model = "Fit",
+                  Year = "2006",
+                  Condition = "Like New",
+                  Price = "6,000",
+                  Availability = "Available",
+                  ExteriorColor = "Red",
+                  InteriorColor = "Black",
+                  Doors = "4",
+                  FuelType = "Gasoline",
+                  Engine = "4 Cylinder",
+                  Transmission = "Manual",
+                  Mileage = "80,000"
+                },
+              new Vehicle   {
+                  VehicleId = 3,
+                  VehicleTitle = "2016 Subaru Outback",
+                  Featured = "true",
+                  Vin = "12345678912345678",
+                  StockNumber = "5678",
+                  Make = "Subaru",
+                  Model = "Outback",
+                  Year = "2016",
+                  Condition = "Like New",
+                  Price = "6,000",
+                  Availability = "Available",
+                  ExteriorColor = "Silver",
+                  InteriorColor = "Black",
+                  Doors = "4",
+                  FuelType = "Gasoline",
+                  Engine = "6 Cylinder",
+                  Transmission = "Automatic",
+                  Mileage = "100,000"
+                }                
+            );
+    }
   }
 }
